ee37dc710892dd58b8aebfda250412ea273a59d2
Brandon Kettle <darkmonkey@phpbb.com>
2007-07-28 01:15:33 +0000

    *** empty log message ***

---
+++ upload/install/index.php
@@ -0,0 +1,333 @@
+<?php
+/** 
+*
+* @mod mod_manager
+* @version $Id$
+* @copyright (c) 2007 phpBB Group 
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
+define('IN_PHPBB', 1);
+define('ADMIN_START', 1);
+define('NEED_SID', true);
+
+// Include files
+$phpbb_root_path = './../';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+require($phpbb_root_path . 'common.' . $phpEx);
+//require($phpbb_root_path . 'includes/functions_admin.' . $phpEx);
+
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup(array('acp/common', 'acp/mods', 'acp/permissions'));
+// End session management
+
+// Did user forget to login? Give 'em a chance to here ...
+if ($user->data['user_id'] == ANONYMOUS)
+{
+	login_box('', $user->lang['LOGIN_ADMIN'], $user->lang['LOGIN_ADMIN_SUCCESS'], true);
+}
+
+// Have they authenticated (again) as an admin for this session?
+if (!isset($user->data['session_admin']) || !$user->data['session_admin'])
+{
+	login_box('', $user->lang['LOGIN_ADMIN_CONFIRM'], $user->lang['LOGIN_ADMIN_SUCCESS'], true, false);
+}
+
+if ((int) $user->data['user_type'] !== USER_FOUNDER)
+{
+	die('Founders only!');
+}
+				
+// We define the admin variables now, because the user is now able to use the admin related features...
+define('IN_ADMIN', true);
+$phpbb_admin_path = './';
+
+// Set custom template for admin area
+$template->set_custom_template($phpbb_root_path . 'install', 'install');
+
+$action = request_var('action', '');
+$submit = (isset($_POST['submit'])) ? true : false;
+
+$template->set_filenames(array(
+	'body' => 'style.html')
+);
+
+$template->assign_vars(array(
+	'SITENAME'						=> $config['sitename'],
+	'PAGE_TITLE'					=> $user->lang['mod_MANAGER'],
+
+	'SID'				=> $SID,
+	'_SID'				=> $_SID,
+	'SESSION_ID'		=> $user->session_id,
+	'ROOT_PATH'			=> $phpbb_root_path,
+
+	'S_USER_LANG'			=> $user->lang['USER_LANG'],
+	'S_USER_BROWSER'		=> (isset($user->data['session_browser'])) ? $user->data['session_browser'] : $user->lang['UNKNOWN_BROWSER'],
+	'S_USERNAME'			=> $user->data['username'],
+	'S_CONTENT_DIRECTION'	=> $user->lang['DIRECTION'],
+	'S_CONTENT_ENCODING'	=> 'UTF-8',
+
+	'T_STYLESHEET_PATH'			=> "{$phpbb_root_path}adm/style/",
+));
+
+
+if (empty($action))
+{
+	// Make Role select
+	$sql = 'SELECT role_id, role_name
+		FROM ' . ACL_ROLES_TABLE . "
+		WHERE role_type = 'a_'
+		ORDER BY role_order ASC";
+	$result = $db->sql_query($sql);
+
+	$s_role_options = '';
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$role_name = (!empty($user->lang[$row['role_name']])) ? $user->lang[$row['role_name']] : $row['role_name'];
+
+		$s_role_options .= '<option value="' . $row['role_id'] . '">' . $role_name . '</option>';
+
+	}
+	$db->sql_freeresult($result);
+
+	$template->assign_vars(array(
+		'S_OVERVIEW'		=> true,
+		
+		'S_ROLE_OPTIONS'	=> $s_role_options,
+		
+		'U_ACTION'			=> append_sid("{$phpbb_root_path}install/index.php?action=install"),
+	));
+}
+elseif ($action = 'install')
+{
+	// check for role id
+	$role_id = request_var('role_id', 0);
+	if ($role_id == 0)
+	{
+		die('give a role you bozo! and make this a proper error message while your at it!');
+	}
+	
+	// Get Actions
+	$actions = array();
+	include_once($phpbb_root_path . 'includes/mod_parser.' . $phpEx);
+	$parser = new parser('xml');
+	$parser->set_file($phpbb_root_path . 'install/install.xml');
+	$actions = $parser->get_actions();
+	unset($parser);
+
+	// Setup editing
+	include($phpbb_root_path . 'includes/editor.' . $phpEx);
+	$editor = new editor($phpbb_root_path);
+	
+	$mod_phpbb_id = '13371337'; //bleh?
+	$edited_root = "install/temp/";
+	
+	// preform file edits
+	if (isset($actions['EDITS']) && !empty($actions['EDITS'])) // this is some beefy looping
+	{
+		$template->assign_vars(array(
+			'S_EDITS'		=> true)
+		);
+		
+		foreach($actions['EDITS'] as $filename => $finds)
+		{
+			if(!file_exists("{$phpbb_root_path}{$filename}"))
+			{
+				$template->assign_block_vars('edit_files', array(
+					'S_MISSING_FILE' => true,
+					
+					'FILENAME'	=> $filename)
+				);
+			}
+			else
+			{
+				$template->assign_block_vars('edit_files', array(
+					'FILENAME'	=> $filename
+				));
+		
+				$file_ext = substr(strrchr(__FILE__, '.'), 1);
+				
+				$editor->open_file($filename);
+
+				$editor->fold_edits($filename, '');
+
+				foreach($finds as $string => $commands)
+				{
+					$template->assign_block_vars('edit_files.finds', array(
+						'FIND_STRING'	=> htmlspecialchars($string)
+					));
+
+					foreach($commands as $type => $contents)
+					{
+						$status = false;
+						$contents_orig = $contents;
+
+						switch(strtoupper($type)) // LANG!
+						{
+							case 'AFTER, ADD':
+								$contents = $editor->add_anchor($contents, $file_ext, $mod_phpbb_id);
+								$status = $editor->add_string($filename, $string, $contents, 'AFTER');
+							break;
+							
+							case 'BEFORE, ADD':
+								$contents = $editor->add_anchor($contents, $file_ext, $mod_phpbb_id);
+								$status = $editor->add_string($filename, $string, $contents, 'BEFORE');
+							break;
+
+							case 'INCREMENT':
+								//$contents = "";
+								$status = $editor->inc_string($filename, $string, $contents);
+							break;
+
+							case 'REPLACE WITH':
+								//$contents = $editor->add_wrap($string, $file_ext, $mod_phpbb_id) . "\n{$contents}";
+								$contents = $editor->add_anchor($contents, $file_ext, $mod_phpbb_id);
+								$status = $editor->replace_string($filename, $string, $contents);
+							break;
+
+							default:
+								die("Error, unrecognised command $type"); // ERROR!
+							break;
+						}
+						
+						$template->assign_block_vars('edit_files.finds.actions', array(
+							'S_SUCCESS'	=> $status,
+
+							'NAME'		=> $type, // LANG!
+							'COMMAND'	=> htmlspecialchars($contents_orig),
+						));
+					}
+				}
+
+				$editor->unfold_edits($filename, '');
+
+				$editor->close_file($filename, "{$edited_root}{$filename}");
+			}
+		}
+	}
+
+	// Preform SQL queries
+	// TODO: need to add the queries further on down to status display
+	if (isset($actions['SQL']) && !empty($actions['SQL']))
+	{
+		$template->assign_vars(array(
+			'S_SQL'		=> true)
+		);
+	
+		foreach($actions['SQL'] as $query)
+		{
+			$db->sql_query($query); // will show general error on fail
+
+			$template->assign_block_vars('sql_queries', array(
+				'S_SUCCESS'	=> true,
+
+				'QUERY'		=> $query,
+			));
+		}
+	}
+
+	// Move edited files back, and delete temp storage folder
+	$editor->copy_content($edited_root, '');
+	$editor->remove($edited_root);
+	
+	// Create mod folder
+	// TODO: .htaccess file needed?
+	$editor->create_dir('mods');
+
+	// Get some Module info
+	$sql = 'SELECT MAX(module_id) as "last_m_id", MAX(right_id) as "last_r_id"
+		FROM ' . MODULES_TABLE ;
+	$result = $db->sql_query($sql);
+	$row = $db->sql_fetchrow($result);
+	$db->sql_freeresult($result);
+
+	// Insert Category Module
+	$module_data = array(
+		'module_id'			=> ($row['last_m_id'] + 1),
+		'module_enabled'	=> 1,
+		'module_display'	=> 1,
+		'module_class'		=> 'acp',
+		'parent_id'			=> 0,
+		'module_langname'	=> 'ACP_CAT_MODS',
+		'left_id'			=> ($row['last_r_id'] + 1),
+		'right_id'			=> ($row['last_r_id'] + 6),
+	);
+					
+	$sql = 'INSERT INTO ' . MODULES_TABLE . ' ' . $db->sql_build_array('INSERT', $module_data);
+	$db->sql_query($sql);
+	
+	// Insert Parent Module
+	$module_data = array(
+		'module_id'			=> ($row['last_m_id'] + 2),
+		'module_enabled'	=> 1,
+		'module_display'	=> 1,
+		'module_class'		=> 'acp',
+		'parent_id'			=> ($row['last_m_id'] + 1),
+		'module_langname'	=> 'ACP_MODS',
+		'left_id'			=> ($row['last_r_id'] + 2),
+		'right_id'			=> ($row['last_r_id'] + 5),
+	);
+					
+	$sql = 'INSERT INTO ' . MODULES_TABLE . ' ' . $db->sql_build_array('INSERT', $module_data);
+	$db->sql_query($sql);
+	
+	// Frontend Module
+	$module_data = array(
+		'module_id'			=> ($row['last_m_id'] + 3),
+		'module_enabled'	=> 1,
+		'module_display'	=> 1,
+		'module_class'		=> 'acp',
+		'parent_id'			=> ($row['last_m_id'] + 2),
+		'module_langname'	=> 'ACP_MOD_MANAGEMENT',
+		'left_id'			=> ($row['last_r_id'] + 3),
+		'right_id'			=> ($row['last_r_id'] + 4),
+		
+		'module_basename'	=> 'mods',
+		'module_mode'		=> 'frontend',
+		'module_auth'		=> 'acl_a_mods',
+	);
+					
+	$sql = 'INSERT INTO ' . MODULES_TABLE . ' ' . $db->sql_build_array('INSERT', $module_data);
+	$db->sql_query($sql);
+
+	// Insert the auth option
+	$auth_data = array(
+		'auth_option_id'	=> '',
+		'auth_option'		=> 'a_mods',
+		'is_global'			=> 1,
+		'is_local'			=> 0,
+		'founder_only'		=> 0,
+	);
+	
+	$sql = 'INSERT INTO ' . ACL_OPTIONS_TABLE . ' ' . $db->sql_build_array('INSERT', $auth_data);
+	$db->sql_query($sql);
+	
+	$auth_option_id = $db->sql_nextid();
+	
+	// Give the wanted role its option
+	$roles_data = array(
+		'role_id'			=> $role_id,
+		'auth_option_id'		=> $auth_option_id,
+		'auth_setting'		=> 1,
+	);
+	
+	$sql = 'INSERT INTO ' . ACL_ROLES_DATA_TABLE . ' ' . $db->sql_build_array('INSERT', $roles_data);
+	$db->sql_query($sql);
+	
+	// Reset cache so we can actaully see the lovely new tab in the ACP
+	$cache->purge();
+
+	$template->assign_vars(array(
+		'S_INSTALL'		=> true,
+		
+		'U_INDEX'				=> append_sid("{$phpbb_root_path}index.$phpEx"),
+	));
+}
+
+
+$template->display('body');
+
+?>
---
+++ upload/language/en/acp/mods.php
@@ -0,0 +1,98 @@
+<?php
+/** 
+*
+* acp_mods [English]
+*
+* @package language
+* @version $Id$
+* @copyright (c) 2005 phpBB Group 
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE 
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+// Banning
+$lang = array_merge($lang, array(
+	'MOD_MANAGER'			=> 'MOD Manager',
+	'INSTALLATION'				=> 'MOD Manager Installation',
+	'INSTALLATION_EXPLAIN'		=> 'Welcome to the MOD Manager Installation. The installer will preform the following steps, and you will need the following information... bleh. Please select a role to apply MOD manager access to, then click install to proceed.',
+	'INSTALLING'				=> 'MOD Manager Installing...',
+	'INSTALLATION_SUCCESSFUL'	=> 'MOD Manager Installed successfully! Please remove this <b>install/</b> directory.',
+	
+	'MODS_EXPLAIN'	=> '<p>Here you can manage the available MODs on your board. MODs allow you to customize your board by automatically installing modifications produced within the phpBB community. For further information on MODs and the MOD Manager please visit the <a href="http://www.phpbb.com/">phpBB website</a>.</p>',
+
+	'NAME'		=> 'Name',
+	'DETAILS'		=> 'Details',
+	
+	'INSTALLED_MODS'		=> 'Installed MODs',
+	'NO_INSTALLED_MODS'		=> 'No installed MODs detected',
+	'UNINSTALLED_MODS'		=> 'Uninstalled MODs',
+	'NO_UNINSTALLED_MODS'	=> 'No uninstalled MODs detected',
+	
+	'MOD_DETAILS'			=> 'MOD Details',
+	'MOD_DETAILS_EXPLAIN'	=> 'Here you can view all known information about the MOD you selected.',
+
+	'PATH'		=> 'Path',
+	'VERSION'		=> 'Version',
+	'DESCRIPTION'	=> 'Description',
+	
+	'AUTHOR_INFORMATION'	=> 'Author Information',
+	'AUTHOR_NOTES'			=> 'Author Notes',
+	'AUTHOR_NAME'			=> 'Author Name',
+	'AUTHOR_EMAIL'			=> 'Author Email',
+	'AUTHOR_URL'			=> 'Author URL',
+	
+	'PRE_INSTALL'			=> 'Preparing to Install',
+	'PRE_INSTALL_EXPLAIN'	=> 'Here you can preview all the modifications to be made to your board, before they are carried out. <strong>WARNING!</strong>, once accepted, your phpBB base files will be edited and database alterations may occur. However, if the install is unsuccessful, assuming you can access the MOD manager, you will be given the option to restore to this point.',
+
+	'UNINSTALL'				=> 'Uninstall',
+	'PRE_UNINSTALL'			=> 'Preparing to Uninstall',
+	'PRE_UNINSTALL_EXPLAIN'	=> 'Here you can preview all the modifications to be made to your board, in order to uninstall the MOD. <strong>WARNING!</strong>, once accepted, your phpBB base files will be edited and database alterations may occur. Also, this process uses reversing techniques that may not be 100% accurate. However, if the uninstall is unsuccessful, assuming you can access the MOD manager, you will be given the option to restore to this point.',
+
+	'NEW_FILES'		=> 'New Files',
+	'SQL_QUERIES'	=> 'SQL Queries',
+	'SOURCE'		=> 'Source',
+	'TARGET'		=> 'Target',
+	'FILE_EDITS'	=> 'File edits',
+	'FIND'			=> 'Find',
+
+	'INSTALLED'			=> 'MOD installed',
+	'INSTALLED_EXPLAIN'	=> 'Your MOD has been installed! Here you can view some of the results from the installation. Please note any errors, and seek support at <a href="http://www.phpbb.com">phpBB.com</a>',
+	
+	'UNINSTALLED'			=> 'MOD uninstalled',
+	'UNINSTALLED_EXPLAIN'	=> 'Your MOD has been uninstalled! Here you can view some of the results from the uninstallation. Please note any errors, and seek support at <a href="http://www.phpbb.com">phpBB.com</a>.',
+
+	'RETURN_MODS'		=> 'Return to the MOD Manager',
+
+	'SUCCESS'		=> 'Success',
+	'ERROR'			=> 'Error',
+	'STATUS'		=> 'Status',
+	
+	'FILE_MISSING'	=> 'Cannot locate file',
+	'FIND_MISSING'	=> 'Cannot locate find string',
+	
+	'REMOVING_FILES'	=> 'Files to be removed',
+	'UNKNOWN_QUERY_REVERSE' => 'Unknown reverse query',
+
+	'ORIGINAL'	=> 'Original',
+	'REVERSE'	=> 'Reverse',
+));
+
+?>
