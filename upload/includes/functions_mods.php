51bb44497f516efadb1b64a8f2a7ad65fbae4b0e
Eric <wgeric@phpbb.com>
2008-02-14 05:20:48 +0000

    I guess I should add this file.

---
+++ upload/includes/functions_mods.php
@@ -0,0 +1,48 @@
+<?php
+/**
+*
+* @package mod_manager
+* @version $Id: functions_admin.php,v 1.254 2007/11/17 12:14:27 acydburn Exp $
+* @copyright (c) 2005 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+function test_ftp_connection($method, &$test_ftp_connection, &$test_connection)
+{
+	global $phpbb_root_path, $phpEx;
+	
+	$transfer = new $method(request_var('host', ''), request_var('username', ''), request_var('password', ''), request_var('root_path', ''), request_var('port', ''), request_var('timeout', ''));
+	
+	$test_connection = $transfer->open_session();
+
+	// Make sure that the directory is correct by checking for the existence of common.php
+	if ($test_connection === true)
+	{
+		// Check for common.php file
+		if (!$transfer->file_exists($phpbb_root_path, 'common.' . $phpEx))
+		{
+			$test_connection = 'ERR_WRONG_PATH_TO_PHPBB';
+		}
+	}
+
+	$transfer->close_session();
+
+	// Make sure the login details are correct before continuing
+	if ($test_connection !== true)
+	{
+		$test_ftp_connection = true;
+	}
+	
+	return;
+}
+
+?>
