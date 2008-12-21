5c50dfc2ed1f21b248d276e71bc2e0bfc6c94932
Igor Wiedler <igor@wiedler.ch>
2008-12-21 13:10:33 +0000

    DEBUG_EXTRA is needed too

--- upload/install/index.php
+++ upload/install/index.php
@@ -13,6 +13,7 @@
 */
 define('IN_PHPBB', true);
 define('DEBUG', true);
+define('DEBUG_EXTRA', true);
 $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
