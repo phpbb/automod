b24ee0a8281ae279be6cd177fa75861593b3b124
Josh Woody <a_jelly_doughnut@phpbb.com>
2008-12-21 16:24:47 +0000

    Tag Beta 1 release

---
+++ 100-b1/install/schemas/automod/mysql_41_schema.sql
@@ -0,0 +1,25 @@
+#
+# $Id$
+#
+
+# Table: 'phpbb_mods'
+CREATE TABLE phpbb_mods (
+	mod_id mediumint(8) UNSIGNED NOT NULL auto_increment,
+	mod_active tinyint(1) UNSIGNED DEFAULT '0' NOT NULL,
+	mod_time int(11) UNSIGNED DEFAULT '0' NOT NULL,
+	mod_dependencies mediumtext NOT NULL,
+	mod_name varchar(100) DEFAULT '' NOT NULL,
+	mod_description text NOT NULL,
+	mod_version varchar(25) DEFAULT '' NOT NULL,
+	mod_author_notes text NOT NULL,
+	mod_author_name varchar(100) DEFAULT '' NOT NULL,
+	mod_author_email varchar(100) DEFAULT '' NOT NULL,
+	mod_author_url varchar(100) DEFAULT '' NOT NULL,
+	mod_actions mediumtext NOT NULL,
+	mod_languages varchar(255) DEFAULT '' NOT NULL,
+	mod_template varchar(255) DEFAULT '' NOT NULL,
+	mod_path varchar(255) DEFAULT '' NOT NULL,
+	PRIMARY KEY (mod_id)
+) CHARACTER SET `utf8` COLLATE `utf8_bin`;
+
+
---
+++ 100-b1/install/schemas/automod/postgres_schema.sql
@@ -0,0 +1,110 @@
+/*
+
+ $Id$
+
+*/
+
+BEGIN;
+
+/*
+	Domain definition
+*/
+CREATE DOMAIN varchar_ci AS varchar(255) NOT NULL DEFAULT ''::character varying;
+
+/*
+	Operation Functions
+*/
+CREATE FUNCTION _varchar_ci_equal(varchar_ci, varchar_ci) RETURNS boolean AS 'SELECT LOWER($1) = LOWER($2)' LANGUAGE SQL STRICT;
+CREATE FUNCTION _varchar_ci_not_equal(varchar_ci, varchar_ci) RETURNS boolean AS 'SELECT LOWER($1) != LOWER($2)' LANGUAGE SQL STRICT;
+CREATE FUNCTION _varchar_ci_less_than(varchar_ci, varchar_ci) RETURNS boolean AS 'SELECT LOWER($1) < LOWER($2)' LANGUAGE SQL STRICT;
+CREATE FUNCTION _varchar_ci_less_equal(varchar_ci, varchar_ci) RETURNS boolean AS 'SELECT LOWER($1) <= LOWER($2)' LANGUAGE SQL STRICT;
+CREATE FUNCTION _varchar_ci_greater_than(varchar_ci, varchar_ci) RETURNS boolean AS 'SELECT LOWER($1) > LOWER($2)' LANGUAGE SQL STRICT;
+CREATE FUNCTION _varchar_ci_greater_equals(varchar_ci, varchar_ci) RETURNS boolean AS 'SELECT LOWER($1) >= LOWER($2)' LANGUAGE SQL STRICT;
+
+/*
+	Operators
+*/
+CREATE OPERATOR <(
+  PROCEDURE = _varchar_ci_less_than,
+  LEFTARG = varchar_ci,
+  RIGHTARG = varchar_ci,
+  COMMUTATOR = >,
+  NEGATOR = >=,
+  RESTRICT = scalarltsel,
+  JOIN = scalarltjoinsel);
+
+CREATE OPERATOR <=(
+  PROCEDURE = _varchar_ci_less_equal,
+  LEFTARG = varchar_ci,
+  RIGHTARG = varchar_ci,
+  COMMUTATOR = >=,
+  NEGATOR = >,
+  RESTRICT = scalarltsel,
+  JOIN = scalarltjoinsel);
+
+CREATE OPERATOR >(
+  PROCEDURE = _varchar_ci_greater_than,
+  LEFTARG = varchar_ci,
+  RIGHTARG = varchar_ci,
+  COMMUTATOR = <,
+  NEGATOR = <=,
+  RESTRICT = scalargtsel,
+  JOIN = scalargtjoinsel);
+
+CREATE OPERATOR >=(
+  PROCEDURE = _varchar_ci_greater_equals,
+  LEFTARG = varchar_ci,
+  RIGHTARG = varchar_ci,
+  COMMUTATOR = <=,
+  NEGATOR = <,
+  RESTRICT = scalargtsel,
+  JOIN = scalargtjoinsel);
+
+CREATE OPERATOR <>(
+  PROCEDURE = _varchar_ci_not_equal,
+  LEFTARG = varchar_ci,
+  RIGHTARG = varchar_ci,
+  COMMUTATOR = <>,
+  NEGATOR = =,
+  RESTRICT = neqsel,
+  JOIN = neqjoinsel);
+
+CREATE OPERATOR =(
+  PROCEDURE = _varchar_ci_equal,
+  LEFTARG = varchar_ci,
+  RIGHTARG = varchar_ci,
+  COMMUTATOR = =,
+  NEGATOR = <>,
+  RESTRICT = eqsel,
+  JOIN = eqjoinsel,
+  HASHES,
+  MERGES,
+  SORT1= <);
+
+/*
+	Table: 'phpbb_mods'
+*/
+CREATE SEQUENCE phpbb_mods_seq;
+
+CREATE TABLE phpbb_mods (
+	mod_id INT4 DEFAULT nextval('phpbb_mods_seq'),
+	mod_active INT2 DEFAULT '0' NOT NULL CHECK (mod_active >= 0),
+	mod_time INT4 DEFAULT '0' NOT NULL CHECK (mod_time >= 0),
+	mod_dependencies TEXT DEFAULT '' NOT NULL,
+	mod_name varchar(100) DEFAULT '' NOT NULL,
+	mod_description varchar(4000) DEFAULT '' NOT NULL,
+	mod_version varchar(25) DEFAULT '' NOT NULL,
+	mod_author_notes varchar(4000) DEFAULT '' NOT NULL,
+	mod_author_name varchar(100) DEFAULT '' NOT NULL,
+	mod_author_email varchar(100) DEFAULT '' NOT NULL,
+	mod_author_url varchar(100) DEFAULT '' NOT NULL,
+	mod_actions TEXT DEFAULT '' NOT NULL,
+	mod_languages varchar(255) DEFAULT '' NOT NULL,
+	mod_template varchar(255) DEFAULT '' NOT NULL,
+	mod_path varchar(255) DEFAULT '' NOT NULL,
+	PRIMARY KEY (mod_id)
+);
+
+
+
+COMMIT;
