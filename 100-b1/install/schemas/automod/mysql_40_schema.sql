fba725b39cbbc364c6fbaa2261dbd3729a38ef7a
Josh Woody <a_jelly_doughnut@phpbb.com>
2008-12-21 16:24:47 +0000

    Tag Beta 1 release

---
+++ 100-b1/install/schemas/automod/mysql_40_schema.sql
@@ -0,0 +1,25 @@
+#
+# $Id$
+#
+
+# Table: 'phpbb_mods'
+CREATE TABLE phpbb_mods (
+	mod_id mediumint(8) UNSIGNED NOT NULL auto_increment,
+	mod_active tinyint(1) UNSIGNED DEFAULT '0' NOT NULL,
+	mod_time int(11) UNSIGNED DEFAULT '0' NOT NULL,
+	mod_dependencies mediumblob NOT NULL,
+	mod_name blob NOT NULL,
+	mod_description blob NOT NULL,
+	mod_version varbinary(25) DEFAULT '' NOT NULL,
+	mod_author_notes blob NOT NULL,
+	mod_author_name blob NOT NULL,
+	mod_author_email blob NOT NULL,
+	mod_author_url blob NOT NULL,
+	mod_actions mediumblob NOT NULL,
+	mod_languages blob NOT NULL,
+	mod_template blob NOT NULL,
+	mod_path blob NOT NULL,
+	PRIMARY KEY (mod_id)
+);
+
+
